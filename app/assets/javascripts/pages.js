f7b6a61a908dd2889a1b6c291bb346d4e9535884
Trevor John, tjohn <pair+trevor@john.tj>
2013-04-06 19:16:51 -0400

    Map has a search field

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,11 +1,42 @@
+function newCenter( address ) {
+  console.log(address)
+
+}
+
+function showZipcodeSearch() {
+  var $zipcodeSearch = $("#zipcode-search")
+
+  if ( $zipcodeSearch.length == 0 ) {
+    var html = "<form id='zipcode-search' class='form-search zip-search center'>" +
+                  "<input type='text' class='input-large search-query' placeholder='Enter a address or zipcode...'>" +
+                  "<button type='submit' class='btn'>Search</button>" +
+              "</form>"
+    $("#map-canvas").after(html)
+
+  } else if ( $zipcodeSearch.is(":hidden") ) {
+    $zipcodeSearch.show()
+  } else {
+    return;
+  }
+
+  var $form = $("form#zipcode-search");
+
+  $form.submit( function (e) {
+    e.preventDefault()
+    newCenter( $(this).children("input").val() )
+  })
+}
+
 function initialize() {
+  var defaultLoc = new google.maps.LatLng(40.718753, -74.006605);
   var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8,
+    center: defaultLoc,
+    zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
-                                mapOptions);
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+  showZipcodeSearch()
 }
 
 $(document).on("page:load", initialize);
