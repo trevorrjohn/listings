7cbe0cfd48d49306aa57786095cd96c7263b2d34
Trevor John, tjohn <pair+trevor@john.tj>
2013-04-06 17:39:11 -0400

    Add google maps

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,13 @@
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"),
+                                mapOptions);
+}
+
+$(document).on("page:load", initialize);
+$(document).ready(initialize)
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Listings::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  root to: 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
